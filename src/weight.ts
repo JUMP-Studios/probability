9744f877a30fe3eeb18eb07d1a6d0432c08694fd
Icejiynn <kamjenkins723@gmail.com>
2023-09-09 02:42:52 -0500

    fix weight typings and logic

--- src/weight.ts
+++ src/weight.ts
@@ -1,13 +1,15 @@
 import Object from "@rbxts/object-utils";
 
 export = {
-	random: (values: Record<string, number>) => {
-		let randomNumber = math.random();
+	random: (values: Record<string, number> | Map<string, number>) => {
+		let randomNumber = new Random().NextNumber()
 
-		for (const [key, weight] of Object.entries(values)) {
+		for (const [key, weight] of Object.entries(values as Record<string, number> )) {
 			randomNumber -= weight;
 
-			if (randomNumber < 0) return key;
+			if (randomNumber <= 0) {
+				return key;
+			}
 		}
 	},
 };
