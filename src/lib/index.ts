bebeb5f1c58eb5df69b0e6d68df05ba27a2a9467
Icejiynn <kamjenkins723@gmail.com>
2023-11-06 01:56:21 -0600

    add random with filterByThreshold function

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,2 @@
 export * as Weight from "./weight"
+export * as Random from "./random"
---
+++ src/lib/random.ts
@@ -0,0 +1,11 @@
+/**
+ * Filter an array of items based on their probability using a threshold.
+ * @param {Array<[string, { probability: number }]>} itemsWithProbability - An array of items with their associated probabilities.
+ * @param {RandomGenerator} random - An instance of a random number generator.
+ * @returns {Array<[string, { probability: number }]>} - Filtered array of items.
+ */
+function filterByThreshold(probabilities: Array<[string, number]>, random: Random) {
+	return probabilities.filter((_key,probability) => random.NextNumber(0, probability) > probability);
+}
+
+export default {filterByThreshold}
