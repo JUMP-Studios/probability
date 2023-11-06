3aed0575ea22ac4d1d92e69d41c7fd85f6feb125
Icejiynn <kamjenkins723@gmail.com>
2023-11-06 02:23:45 -0600

    correct weight luck skew function

--- src/lib/weight.ts
+++ src/lib/weight.ts
@@ -3,20 +3,24 @@ import Object from "@rbxts/object-utils";
 type WeightObject = Record<string, number>
 
 function skew(baseRates: WeightObject, luckFactor: number) {
-	const skewedRates = { ...baseRates };
+	// [-1,1]
+	luckFactor = math.min(1, math.max(-1, luckFactor));
+
+    const skewedRates: Record<string, number> = {};
 	const totalWeight = Object.values(skewedRates).reduce((sum, rate) => sum + rate, 0); // Total weight of all entries
 	const adjustmentFactor = luckFactor ** 2; // Calculate the factor to adjust the probabilities
 	const keys = Object.keys(skewedRates)
   
-	// Adjust the probabilities of each entry
-	keys.forEach((entry) => {
-		skewedRates[entry] *= adjustmentFactor;
+	keys.forEach((key) => {
+		const originalRate = baseRates[key];
+		const newRate = originalRate * (1 + luckFactor);
+		skewedRates[key] = math.max(0, newRate); // Ensure rates are non-negative
 	})
   
 	// Normalize the probabilities to ensure they still sum up to 1
-	const normalizedTotalWeight = Object.values(skewedRates).reduce((sum, rate) => sum + rate, 0);
+    const totalRate = Object.values(skewedRates).reduce((total, rate) => total + rate, 0);
 	keys.forEach((entry) => {
-		skewedRates[entry] /= normalizedTotalWeight / totalWeight;
+		skewedRates[entry] /= totalRate / totalWeight;
 	})
   
 	return skewedRates;
